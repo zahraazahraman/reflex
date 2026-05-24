67155938f24a773177129d50e38dfe34e78393d1
Zahraa Zahraman <zahraazahraman@gmail.com>
2026-05-24 15:43:30 +0300

    Initial commit

---
+++ Reflex/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Reflex
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Reflex/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Reflex
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
